445c6f4b98fbfed0a5166ec0e50412ae1b07f86f
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-11-19 11:35:50 +0100

    Adapted run script

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for x in `ls zrodla` ; do
-    cp zrodla/$x src/mm1.c
-    cmake -DPAPI_PREFIX=/home/rikkt0r/out
-    make
-    echo "------  $x  -------"
-    ./OORA
+cmake -DPAPI_PREFIX=/home/rikkt0r/out
+make
+for x in 1 2 3 4 5 51 52 6 7 8 9 ; do
+    echo "------ Solver $x TIME -------"
+    ./OORA ${x} 0
+    echo "------ Solver $x FLOPS 1 -------"
+    ./OORA ${x} 1
+    echo "------ Solver $x FLOPS 2 -------"
+    ./OORA ${x} 2
 done
