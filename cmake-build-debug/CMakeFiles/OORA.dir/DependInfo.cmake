55f805b100fe1548d4bd52fa36998d9ae9b5c9a7
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-11-20 18:40:24 +0100

    Adds results

---
+++ cmake-build-debug/CMakeFiles/OORA.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kbhit/Sources/OORA/src/main.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/main.c.o"
+  "/Users/kbhit/Sources/OORA/src/measurements.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/measurements.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm1.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm1.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm2.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm2.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm3.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm3.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm4.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm4.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm5.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm5.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm5a.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm5a.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm5b.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm5b.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm6.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm6.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm7.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm7.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm8.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm8.c.o"
+  "/Users/kbhit/Sources/OORA/src/mm9.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/mm9.c.o"
+  "/Users/kbhit/Sources/OORA/src/performance.c" "/Users/kbhit/Sources/OORA/cmake-build-debug/CMakeFiles/OORA.dir/src/performance.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OORA.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OORA.dir/src/main.c.o"
+  "CMakeFiles/OORA.dir/src/mm1.c.o"
+  "CMakeFiles/OORA.dir/src/mm2.c.o"
+  "CMakeFiles/OORA.dir/src/mm3.c.o"
+  "CMakeFiles/OORA.dir/src/mm4.c.o"
+  "CMakeFiles/OORA.dir/src/mm5.c.o"
+  "CMakeFiles/OORA.dir/src/mm5a.c.o"
+  "CMakeFiles/OORA.dir/src/mm5b.c.o"
+  "CMakeFiles/OORA.dir/src/mm6.c.o"
+  "CMakeFiles/OORA.dir/src/mm7.c.o"
+  "CMakeFiles/OORA.dir/src/mm8.c.o"
+  "CMakeFiles/OORA.dir/src/mm9.c.o"
+  "CMakeFiles/OORA.dir/src/performance.c.o"
+  "CMakeFiles/OORA.dir/src/measurements.c.o"
+  "OORA.pdb"
+  "OORA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OORA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
